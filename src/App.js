c175f7b596e33b9a476e05dc09c708393727dcac
vaniammu@123 <vanimanoj1997@gmail.com>
2022-05-18 01:31:37 +0530

    firstcommit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 
 function App() {
   return (
